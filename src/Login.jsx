7c789ea9bcb2c8de934df050aa29dd93ccf6af82
Akira509 <priyakadyan509@gmail.com>
2025-07-28 11:58:11 +0530

    case change

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./auth.css";
+import "./Auth.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
